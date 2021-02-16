7b9b623706fe31887a394e5a534bda16e4ebed54
adrienclesse <adrienclesse@hotmail.com>
2021-02-16 16:21:33 +0100

    finishing the todo list app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./components/TodoList";
+
+
+
+function App() {
+
+
+  return (
+    
+      <div class='todo-app'>
+      <TodoList />
+      </div>
+    
+  );
+}
+
+export default App;
